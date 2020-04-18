d48799fb0d03f42420433b94e17e412ee893c77f
SvenLuttik <61277332+SvenLuttik@users.noreply.github.com>
2020-04-18 20:32:45 +0200

    eindelijk iets werkend
    idk hoe

--- javascript.js
+++ javascript.js
@@ -1,17 +1,63 @@
 
-// init map
-var myMap;
+	mapboxgl.accessToken = 'pk.eyJ1Ijoic3ZlbnRleSIsImEiOiJjazk1djAzZ2IwMmQ2M2Z0YzlocjFwNGh0In0.9HbUhvs2jWClA1vkLYutzg';
+    var map = new mapboxgl.Map({
+        style: 'mapbox://styles/mapbox/light-v10',
+        center: [4.768893, 52.498374],
+        zoom: 17.5,
+        pitch: 45,
+        bearing: -17.6,
+        container: 'map',
+        antialias: true
+    });
 
-function initMap() {
-	// set options for map 
-	var mapOptions = {
-		center: {
-			lat: 52.497997, 
-			lng: 4.768848
-		},
-		zoom: 17,
-	};
+    // The 'building' layer in the mapbox-streets vector source contains building-height
+    // data from OpenStreetMap.
+    map.on('load', function() {
+        // Insert the layer beneath any symbol layer.
+        var layers = map.getStyle().layers;
 
-	// create map and add to page
-	myMap = new google.maps.Map(document.getElementById('map'), mapOptions);
-}
+        var labelLayerId;
+        for (var i = 0; i < layers.length; i++) {
+            if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
+                labelLayerId = layers[i].id;
+                break;
+            }
+        }
+
+        map.addLayer(
+            {
+                'id': '3d-buildings',
+                'source': 'composite',
+                'source-layer': 'building',
+                'filter': ['==', 'extrude', 'true'],
+                'type': 'fill-extrusion',
+                'minzoom': 15,
+                'paint': {
+                    'fill-extrusion-color': '#aaa',
+
+                    // use an 'interpolate' expression to add a smooth transition effect to the
+                    // buildings as the user zooms in
+                    'fill-extrusion-height': [
+                        'interpolate',
+                        ['linear'],
+                        ['zoom'],
+                        15,
+                        0,
+                        15.05,
+                        ['get', 'height']
+                    ],
+                    'fill-extrusion-base': [
+                        'interpolate',
+                        ['linear'],
+                        ['zoom'],
+                        15,
+                        0,
+                        15.05,
+                        ['get', 'min_height']
+                    ],
+                    'fill-extrusion-opacity': 0.6
+                }
+            },
+            labelLayerId
+        );
+    });
