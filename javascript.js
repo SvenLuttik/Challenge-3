ba35dc16d30aed6c18a2934eb501338a49142255
SvenLuttik <61277332+SvenLuttik@users.noreply.github.com>
2020-04-19 13:00:54 +0200

    key update

--- javascript.js
+++ javascript.js
@@ -115,7 +115,7 @@ weather.temperature = {
 // APP CONSTS AND VARS
 const KELVIN = 273;
 // API KEY
-const key = "82005d27a116c2880c8f0fcb866998a0";
+const key = "e384e1930acfff1d3f8dc1fbafe3752b";
 
 // CHECK IF BROWSER SUPPORTS GEOLOCATION
 if('geolocation' in navigator){
