547c818832754aead5620215ae10368af9f28842
SvenLuttik <61277332+SvenLuttik@users.noreply.github.com>
2020-04-20 11:55:26 +0200

    style update

--- javascript.js
+++ javascript.js
@@ -1,67 +1,64 @@
+//MAPBOX API
+mapboxgl.accessToken = 'pk.eyJ1Ijoic3ZlbnRleSIsImEiOiJjazk1djAzZ2IwMmQ2M2Z0YzlocjFwNGh0In0.9HbUhvs2jWClA1vkLYutzg';
+var map = new mapboxgl.Map({
+    style: 'mapbox://styles/mapbox/light-v10',
+    center: [4.768890, 52.498012],
+    zoom: 17.5,
+    pitch: 45,
+    bearing: -17.6,
+    container: 'map',
+    antialias: true
+});
 
-	mapboxgl.accessToken = 'pk.eyJ1Ijoic3ZlbnRleSIsImEiOiJjazk1djAzZ2IwMmQ2M2Z0YzlocjFwNGh0In0.9HbUhvs2jWClA1vkLYutzg';
-    var map = new mapboxgl.Map({
-        style: 'mapbox://styles/mapbox/light-v10',
-        center: [4.768890, 52.498012],
-        zoom: 17.5,
-        pitch: 45,
-        bearing: -17.6,
-        container: 'map',
-        antialias: true
-    });
 
-    // The 'building' layer in the mapbox-streets vector source contains building-height
-    // data from OpenStreetMap.
-    map.on('load', function() {
-        // Insert the layer beneath any symbol layer.
-        var layers = map.getStyle().layers;
+map.on('load', function() {
+    var layers = map.getStyle().layers;
 
-        var labelLayerId;
-        for (var i = 0; i < layers.length; i++) {
-            if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
-                labelLayerId = layers[i].id;
-                break;
-            }
+    var labelLayerId;
+    for (var i = 0; i < layers.length; i++) {
+         if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
+              labelLayerId = layers[i].id;
+            break;
         }
+    }
 
-        map.addLayer(
-            {
-                'id': '3d-buildings',
-                'source': 'composite',
-                'source-layer': 'building',
-                'filter': ['==', 'extrude', 'true'],
-                'type': 'fill-extrusion',
-                'minzoom': 15,
-                'paint': {
-                    'fill-extrusion-color': '#aaa',
+    map.addLayer(
+        {
+            'id': '3d-buildings',
+            'source': 'composite',
+            'source-layer': 'building',
+            'filter': ['==', 'extrude', 'true'],
+            'type': 'fill-extrusion',
+            'minzoom': 15,
+            'paint': {
+                'fill-extrusion-color': '#aaa',
 
-                    // use an 'interpolate' expression to add a smooth transition effect to the
-                    // buildings as the user zooms in
-                    'fill-extrusion-height': [
-                        'interpolate',
-                        ['linear'],
-                        ['zoom'],
-                        15,
-                        0,
-                        15.05,
-                        ['get', 'height']
-                    ],
-                    'fill-extrusion-base': [
-                        'interpolate',
-                        ['linear'],
-                        ['zoom'],
-                        15,
-                        0,
-                        15.05,
-                        ['get', 'min_height']
-                    ],
-                    'fill-extrusion-opacity': 0.6
-                }
-            },
-            labelLayerId
-        );
-    });
+                'fill-extrusion-height': [
+                    'interpolate',
+                    ['linear'],
+                    ['zoom'],
+                    15,
+                    0,
+                    15.05,
+                    ['get', 'height']
+                ],
+                'fill-extrusion-base': [
+                    'interpolate',
+                    ['linear'],
+                    ['zoom'],
+                    15,
+                    0,
+                    15.05,
+                    ['get', 'min_height']
+                ],
+                'fill-extrusion-opacity': 0.6
+            }
+        },
+        labelLayerId
+    );
+});
 
+//MAPBOX ICONS
 map.on('load', function() {
         map.loadImage( 'https://raw.githubusercontent.com/SvenLuttik/Challenge-3/master/images/marker.png',
             function(error, image) {
@@ -95,96 +92,31 @@ map.on('load', function() {
         );
     });
 
-// Tutorial by http://youtube.com/CodeExplained
-// api key : 82005d27a116c2880c8f0fcb866998a0
 
-// SELECT ELEMENTS
-const iconElement = document.querySelector(".weather-icon");
-const tempElement = document.querySelector(".temperature-value p");
-const descElement = document.querySelector(".temperature-description p");
-const locationElement = document.querySelector(".location p");
-const notificationElement = document.querySelector(".notification");
+//OPEN WEATHER API
+var input = document.querySelector(".inputTekst");
+var knop = document.querySelector(".knop");
+var temperatuur = document.querySelector(".temperatuur");
+var beschrijving = document.querySelector(".beschrijving");
+var locatie = document.querySelector(".locatie");
+var icoon = document.querySelector(".icoon");
 
-// App data
-const weather = {};
-
-weather.temperature = {
-    unit : "celsius"
-}
-
-// APP CONSTS AND VARS
-const KELVIN = 273;
-// API KEY
-const key = "e384e1930acfff1d3f8dc1fbafe3752b";
-
-// CHECK IF BROWSER SUPPORTS GEOLOCATION
-if('geolocation' in navigator){
-    navigator.geolocation.getCurrentPosition(setPosition, showError);
-}else{
-    notificationElement.style.display = "block";
-    notificationElement.innerHTML = "<p>Browser doesn't Support Geolocation</p>";
-}
-
-// SET USER'S POSITION
-function setPosition(position){
-    let latitude = position.coords.latitude;
-    let longitude = position.coords.longitude;
+knop.addEventListener('click',function(name){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=e384e1930acfff1d3f8dc1fbafe3752b')
+    .then(response => response.json())
+    .then(data => {
+        var locatieValue = data['name'];
+        var temperatuurValue = data['main']['temp'];
+        var beschrijvingValue = data['weather'][0]['description'];
+        var icoonValue = data['weather'][0]['icon'];
     
-    getWeather(latitude, longitude);
-}
-
-// SHOW ERROR WHEN THERE IS AN ISSUE WITH GEOLOCATION SERVICE
-function showError(error){
-    notificationElement.style.display = "block";
-    notificationElement.innerHTML = `<p> ${error.message} </p>`;
-}
-
-// GET WEATHER FROM API PROVIDER
-function getWeather(latitude, longitude){
-    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+        locatie.innerHTML = locatieValue;
+        temperatuur.innerHTML = Math.round(temperatuurValue - 272.15) + " -° C";
+        beschrijving.innerHTML = beschrijvingValue;
+        icoon.innerHTML = `<img src="icons/${icoonValue}.png"/>`;
     
-    fetch(api)
-        .then(function(response){
-            let data = response.json();
-            return data;
-        })
-        .then(function(data){
-            weather.temperature.value = Math.floor(data.main.temp - KELVIN);
-            weather.description = data.weather[0].description;
-            weather.iconId = data.weather[0].icon;
-            weather.city = data.name;
-            weather.country = data.sys.country;
-        })
-        .then(function(){
-            displayWeather();
-        });
-}
+    })
 
-// DISPLAY WEATHER TO UI
-function displayWeather(){
-    iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
-    tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
-    descElement.innerHTML = weather.description;
-    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
-}
+    .catch(err => alert("404. Kan de locatie niet vinden"))
 
-// C to F conversion
-function celsiusToFahrenheit(temperature){
-    return (temperature * 9/5) + 32;
-}
-
-// WHEN THE USER CLICKS ON THE TEMPERATURE ELEMENET
-tempElement.addEventListener("click", function(){
-    if(weather.temperature.value === undefined) return;
-    
-    if(weather.temperature.unit == "celsius"){
-        let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
-        fahrenheit = Math.floor(fahrenheit);
-        
-        tempElement.innerHTML = `${fahrenheit}°<span>F</span>`;
-        weather.temperature.unit = "fahrenheit";
-    }else{
-        tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
-        weather.temperature.unit = "celsius"
-    }
-});
+})
